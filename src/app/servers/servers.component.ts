6afa066b4ab816ef50c8704465c0e7c2670cec1f
Anuj Kumar <87.anuj.kumar@gmail.com>
2019-09-10 21:58:04 +0530

    Two way Databinding uasge

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -15,7 +15,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = "No Server was created!"
-  serverName ='';
+  serverName ='Test Server';
 
   constructor() { 
     setTimeout(() => { this.allowNewServer = true; },1000);
